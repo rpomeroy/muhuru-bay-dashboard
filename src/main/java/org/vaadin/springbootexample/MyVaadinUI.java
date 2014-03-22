d9ae2d0c4ccafb280b8f01a9cd06468185c2c531
Ron Pomeroy <rpomeroy@me.com>
2014-03-22 13:43:17 -0500

    Inital bootstrapping of a Spring Boot Vaadin app.  It launches with with a single button

---
+++ src/main/java/org/vaadin/springbootexample/MyVaadinUI.java
@@ -0,0 +1,39 @@
+package org.vaadin.springbootexample;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyVaadinUI extends UI implements Button.ClickListener{
+    
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+    private MyService service;
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+
+        Button button = new Button("Click Me");
+        button.addClickListener(this);
+        
+        layout.addComponent(button);
+
+        setContent(layout);
+    }
+
+	@Override
+	public void buttonClick(ClickEvent event) {
+		Notification.show(service.sayHello());
+		
+	}
+}
